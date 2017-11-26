d75a503844322b542238b2f99a0a0264ae3717eb
Isaac Yoon <ysaacioon@yahoo.com>
2017-11-25 20:20:55 -0800

    add server side code

--- client/reducers/HeaderReducer.js
+++ client/reducers/HeaderReducer.js
@@ -1,4 +1,4 @@
-,op.[import data from '../mock.json'
+import data from '../mock.json'
 
 const INITIAL_STATE = {
   name:'',
@@ -10,6 +10,7 @@ export default (state = INITIAL_STATE, action) => {
   console.log('10', state, action)
   switch (action.type) {
     case 'clientData': 
+    //
       return { clientData: action.payload };
     default:
       return state;
