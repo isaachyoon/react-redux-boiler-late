9f1e68775921f07f569566b6ae6630f82c1330ea
Isaac Yoon <ysaacioon@yahoo.com>
2017-12-05 18:14:07 -0800

    auto hot reload

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const path = require('path');
-
+const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
   template: './client/index.html',
@@ -7,17 +7,29 @@ const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
   inject: 'body'
 })
 
+var BUILD_DIR = path.join(__dirname, 'client/public');
+
+
 module.exports = {
   entry: './client/index.js',
   output: {
-    path: path.resolve('dist'),
+    path: BUILD_DIR,
     filename: 'index_bundle.js'
   },
   module: {
     loaders: [
-      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
-      { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
+      { test: /\.jsx?$/, 
+        loader: 'babel-loader', 
+        exclude: /node_modules/
+      },
     ]
   },
-  plugins: [HtmlWebpackPluginConfig]
+  plugins: [
+    HtmlWebpackPluginConfig,
+    // new webpack.HotModuleReplacementPlugin(),
+    // new webpack.NoErrorsPlugin()
+  ], 
+  devServer: {
+    headers: { "Access-Control-Allow-Origin": "*" }
+  },
 }
