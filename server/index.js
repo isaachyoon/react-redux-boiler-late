70f0e9bcd200bc67231419cb07a3e369522e5576
Isaac Yoon <ysaacioon@yahoo.com>
2017-12-05 18:13:52 -0800

    serve up asset from server

--- server/index.js
+++ server/index.js
@@ -1,8 +1,20 @@
+// var app = require('./basic_server.js')
 const express = require('express')
 const app = express();
+var path = require('path')
+// 
+// app.get('/', (req, res) => {
+  // res.send('Hello World')
+// })
 
 app.get('/', (req, res) => {
-  res.send('Hello World')
+  app.use(express.static(path.join(__dirname, '../client')))
+  res.sendFile(path.join(__dirname, '/../client/index.html'))
+})
+
+app.get('/login', function(req, res) {
+  console.log('request from ');
+  res.send('what up')
 })
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
