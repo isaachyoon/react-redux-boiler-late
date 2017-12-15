d6639aa1a1b35d48aa086d99f52a12a095ef2327
Isaac Yoon <ysaacioon@yahoo.com>
2017-12-14 17:11:37 -0800

    modify index.js

--- client/components/Home/index.js
+++ client/components/Home/index.js
@@ -26,7 +26,8 @@ class Header extends React.Component {
       this.setState({
         name: evt.target.value
       })
-    } if(type === 'number') {
+    } 
+    if(type === 'number') {
       this.setState({
         number: evt.target.value
       })
@@ -38,7 +39,6 @@ class Header extends React.Component {
       <div className="container">
       <div>
         <p>What is Lorem Ipsum? you tell me</p>
-        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
       </div>
       <div>
         Name: <input type="text" value = {this.state.name} name="fname" onChange={this.updateInputValue.bind(this, 'name')}/> 
